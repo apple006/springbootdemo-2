c510dec9d83f23d0fb647229062ce2d3130cf113
chenquan <18975233382@163.com>
2018-04-04 17:35:57 +0800

    springboot + mybatis 整合

--- src/main/java/com/chenquan/controller/HelloController.java
+++ src/main/java/com/chenquan/controller/HelloController.java
@@ -10,7 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    public static void main(String[] args) {
-        
-    }
+  
 }
